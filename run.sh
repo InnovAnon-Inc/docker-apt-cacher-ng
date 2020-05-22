57ea3828a8a9079c34eb3bfc278e64f537e277bf
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-22 02:02:09 -0500

    auto update

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 set -exu
 [ $# -eq 0 ]
+
+sudo             -- \
+nice -n +20      -- \
+sudo -u `whoami` -- \
 docker-compose build
+
 docker push innovanon/docker-apt-cacher-ng:latest || :
+
+sudo             -- \
+nice -n +20      -- \
+sudo -u `whoami` -- \
 docker-compose up --force-recreate -d
 
